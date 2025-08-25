9bd844278aaabc5982d2687e99e18b377ac1337e
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Air.local>
2025-08-26 01:11:52 +0530

    trial

---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <numeric>      // gcd, accumulate
+#include <functional>
+#include <iomanip>      // setprecision
+#include <sstream>
+#include <tuple>
+#include <bitset>
+#include <limits>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+
+signed main() {
+    fastIO();
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+       cout << "Trying" << endl;
+    }
+
+    return 0;
+}
