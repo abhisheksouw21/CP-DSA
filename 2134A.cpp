d9d6c4811527cae8e6faf910b70306d4e4a6261d
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Air.local>
2025-08-27 01:03:29 +0530

    contest

---
+++ 2134A.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <numeric>      // gcd, accumulate
+#include <functional>
+#include <iomanip>      // setprecision
+#include <sstream>
+#include <tuple>
+#include <bitset>
+#include <limits>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+// #define int long long
+// #define endl '\n'
+// #define pb push_back
+// #define all(x) (x).begin(), (x).end()
+// #define sz(x) (int)(x).size()
+
+
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+
+signed main() {
+    fastIO();
+
+    int t ;
+    cin >> t;
+    while (t--) {
+    int n,a,b;
+    cin >> n >> a >> b;
+    if(a<=b){
+        if((n-b)%2!=0){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+    else{
+        if((n-b)%2!=0 || (n-a)%2!=0){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+    
+        }
+    } 
