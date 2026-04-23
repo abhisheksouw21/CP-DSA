ad7c5a620b1bee2ffdd482a09e498a99741b76b6
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-04-23 02:55:04 +0000

    Add Minimum Increment to Make Array Unique from LeetCode

---
+++ LeetCode/Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       int ans=0;
+       for(int i=1;i<nums.size();++i){
+        if(nums[i-1]==nums[i]){
+            nums[i]++;
+            ans++;
+        }
+       else if(nums[i-1]>nums[i]){
+          ans+= (nums[i-1]-nums[i])+1;
+          nums[i]+=(nums[i-1]-nums[i])+1;
+        }
+        else {
+            continue;
+        }
+       }
+       return ans; 
+    }
+};
