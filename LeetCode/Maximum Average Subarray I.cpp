2ea5464e8b94009896d0b5900e48f50c76904f3f
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-04-25 02:45:28 +0000

    Add Maximum Average Subarray I from LeetCode

---
+++ LeetCode/Maximum Average Subarray I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int sum=0;
+        double ans=INT_MIN;
+        while(j<nums.size()){
+            sum+=nums[j];
+            if(j-i+1<k){
+               j++;
+            }
+            else{
+              ans = max(ans, (double)sum / k);
+                sum-=nums[i];
+                i++;
+                j++;    
+            }
+        }
+        return ans;
+    }
+};
