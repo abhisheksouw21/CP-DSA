641ac5da81ef9a4e9b024776095467b34474f9d6
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-05-03 03:35:14 +0000

    Add Check If It Is a Straight Line from LeetCode

---
+++ LeetCode/Check If It Is a Straight Line.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int x1 = coordinates[0][0];
+        int y1 = coordinates[0][1];
+        int x2 = coordinates[1][0];
+        int y2 = coordinates[1][1];
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        for (int i = 2; i < coordinates.size(); ++i) {
+            int x3 = coordinates[i][0];
+            int y3 = coordinates[i][1];
+
+            if (dy * (x3 - x1) != dx * (y3 - y1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
