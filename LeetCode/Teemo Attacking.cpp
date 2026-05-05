409146a379a793e2c4c579af3a4d28d63f402b43
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-05-05 03:20:35 +0000

    Add Teemo Attacking from LeetCode

--- LeetCode/Teemo Attacking.cpp
+++ LeetCode/Teemo Attacking.cpp
@@ -3,14 +3,7 @@ public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         int ans=duration;
         for(int i=0;i<timeSeries.size()-1;++i){
-            int temp=timeSeries[i]+duration -1;
-            if(temp<timeSeries[i+1]){
-                ans+=duration;
-            }
-            else{
-                ans+=duration;
-                ans-=(temp-timeSeries[i+1]+1);
-            }
+            ans+=min(timeSeries[i+1]-timeSeries[i],duration);
         }
         return ans; 
     }
