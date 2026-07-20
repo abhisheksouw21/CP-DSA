37200974d3b65da57f375b874fb7a4ffd4a81781
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-07-20 03:30:03 +0000

    Add Smallest Subsequence of Distinct Characters from LeetCode

---
+++ LeetCode/Smallest Subsequence of Distinct Characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int>v(26,0);
+        vector<bool>a(26,false);
+        string result ="";
+        for(int i=0;i<s.size();++i){
+            v[s[i]-'a']=i;
+        }
+         for(int i=0;i<s.size();++i){
+           char c=s[i];
+           if(a[c-'a'])continue;
+           while(!result.empty() && result.back()>c && v[result.back()-'a']>i){
+            a[result.back()-'a']=false;
+            result.pop_back();
+           }
+           result.push_back(c);
+            a[c-'a']=true;
+        }
+        return result; 
+    }
+};
