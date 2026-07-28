7b79a66fac95d833347fa1942745cf7ab6831eff
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-07-28 02:44:06 +0000

    Add Binary Tree Right Side View from LeetCode

--- LeetCode/Binary Tree Right Side View.cpp
+++ LeetCode/Binary Tree Right Side View.cpp
@@ -10,24 +10,33 @@
  * };
  */
 class Solution {
+     void f(TreeNode* root,int l,vector<int>&ans){
+        if(root==nullptr) return;
+        if(ans.size()==l) ans.push_back(root->val);
+        f( root->right,l+1,ans);
+        f( root->left,l+1,ans);
+    }
 public:
     vector<int> rightSideView(TreeNode* root) {
-         vector<int> ans;
-        if(root==nullptr) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>temp;
-            for(int i=0;i<size;++i){
-                TreeNode* node= q.front();
-                q.pop();
-                if(node->left!=nullptr) q.push(node->left);
-                  if(node->right!=nullptr) q.push(node->right);
-                  temp.push_back(node->val);
-            }
-            ans.push_back(temp[size-1]);
-        }
-        return ans;
+        vector<int>ans;
+         f(root,0,ans);
+         return ans;
+        //  vector<int> ans;
+        // if(root==nullptr) return ans;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     vector<int>temp;
+        //     for(int i=0;i<size;++i){
+        //         TreeNode* node= q.front();
+        //         q.pop();
+        //         if(node->left!=nullptr) q.push(node->left);
+        //           if(node->right!=nullptr) q.push(node->right);
+        //           temp.push_back(node->val);
+        //     }
+        //     ans.push_back(temp[size-1]);
+        // }
+        // return ans;
     }
 };
