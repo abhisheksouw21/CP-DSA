7767d8d3106364e80a30aa081b76a8f8192e0c9b
abhisheksouw21 <abhishek_240294@aitpune.edu.in>
2026-08-02 03:15:45 +0000

    Add Count Valid Prefixes from LeetCode

---
+++ LeetCode/Count Valid Prefixes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int ans=0;
+        int c0=0;
+        int c1=0;
+        for(char c:s){
+            if(c=='0')c0++;
+            else c1++;
+           if(abs(c0-c1)<=1){
+               ans++;
+           } 
+        }
+        return ans;
+    }
+};
