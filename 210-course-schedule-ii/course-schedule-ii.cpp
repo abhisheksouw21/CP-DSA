b7494e80a8f6d1a0d04c04ebea9450af963b894a
Abhishek Abhishek <abhishek_240294@aitpune.edu.in>
2026-09-22 23:40:43 +0530

    Time: 7 ms (34.71%) | Memory: 19.1 MB (21.72%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+      vector<vector<int>> constructadj(int V, vector<vector<int>>& edges) {
+                vector<vector<int>> adj(V);
+                for (auto it : edges) {
+                    adj[it[0]].push_back(it[1]); 
+                }
+                return adj;
+                  }
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+         vector<vector<int>> adj = constructadj(n, pre);
+         vector<int> indegree(n, 0);
+             queue<int> q;
+             vector<int> list;
+         for (int i = 0; i < n; i++) {
+                for (int next : adj[i])
+                    indegree[next]++;
+            }
+
+            
+            for (int i = 0; i < n; i++)
+                if (indegree[i] == 0)
+                    q.push(i);
+
+           
+            while (!q.empty()) {
+                int top = q.front();
+                q.pop();
+                list.push_back(top);
+                for (int next : adj[top]) {
+                    indegree[next]--;
+                    if (indegree[next] == 0)
+                        q.push(next);
+                }
+            }
+            reverse(list.begin(), list.end());
+        if(list.size()==n) return list;
+        return {};
+    }
+};
